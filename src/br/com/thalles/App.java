144dcda6132615ed2646bc3f953d80b920075830
Thalles <thallesrafaell@outlook.com>
2024-01-07 11:56:16 -0300

    Adicionando funções de Excluir e Altear Cliente

--- src/br/com/thalles/App.java
+++ src/br/com/thalles/App.java
@@ -41,6 +41,16 @@ public class App {
                 String dados = JOptionPane.showInputDialog(null, "Digite o CPF para consultar os dados do Cliente",
                         "Consultar", JOptionPane.INFORMATION_MESSAGE);
                consultar(dados);
+            } else if (isEcluir(opcao)) {
+                String dados = JOptionPane.showInputDialog(null,
+                        "Digite o CPF do cliente",
+                        "Consulta cliente", JOptionPane.INFORMATION_MESSAGE);
+                excluir(dados);
+            } else if (isAlterar(opcao)) {
+                String dados = JOptionPane.showInputDialog(null,
+                        "Digite os dados do cliente separados por vígula, conforme exemplo: Nome, CPF, Telefone, Endereço, Número, Cidade e Estado",
+                        "Atualização", JOptionPane.INFORMATION_MESSAGE);
+                alterar(dados);
             }
             opcao = JOptionPane.showInputDialog(null,
                     "Digite 1 para cadastro, 2 para consulta, 3 para excluir, 4 para alterar ou 5 para sair",
@@ -48,6 +58,18 @@ public class App {
         }
 
     }
+
+    private static void alterar(String dados) {
+        String[] dadosSeparados = dados.split(",");
+        Cliente cliente =new Cliente(dadosSeparados[0],dadosSeparados[1],dadosSeparados[2],dadosSeparados[3],dadosSeparados[4],dadosSeparados[5],dadosSeparados[6]);
+        iClienteDAO.alterar(cliente);
+    }
+
+    private static void excluir(String dados) {
+        iClienteDAO.excluir(Long.parseLong(dados));
+        JOptionPane.showMessageDialog(null, "Cliente excluído com sucesso: ", "Sucesso",JOptionPane.INFORMATION_MESSAGE);
+    }
+
 
     private static void consultar(String dados) {
         Cliente cliente = iClienteDAO.consultar(Long.parseLong(dados));
@@ -58,12 +80,8 @@ public class App {
         }
 
     }
-    private static boolean isConsultar(String opcao) {
-        if("2".equals(opcao)){
-            return true;
-        }
-        return false;
-    }
+
+
 
     private static void cadastrar(String dados) {
         String[] dadosSeparados = dados.split(",");
@@ -76,12 +94,6 @@ public class App {
         }
     }
 
-    private static boolean isOpcaoSair(String opcao) {
-        if("5".equals(opcao)){
-            return true;
-        }
-        return false;
-    }
 
 
     private static void sair() {
@@ -89,8 +101,29 @@ public class App {
         System.exit(0);
     }
 
-    private static boolean isOpcaoValida(String opcao) {
-        if("1".equals(opcao) || "2".equals(opcao) || "3".equals(opcao) || "4".equals(opcao) || "5".equals(opcao)){
+    private static boolean isOpcaoSair(String opcao) {
+        if("5".equals(opcao)){
+            return true;
+        }
+        return false;
+    }
+
+   private  static boolean isAlterar(String opcao){
+        if("4".equals(opcao)){
+            return true;
+        }
+        return false;
+   }
+
+    private static boolean isEcluir(String opcao) {
+        if ("3".equals(opcao)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isConsultar(String opcao) {
+        if("2".equals(opcao)){
             return true;
         }
         return false;
@@ -104,4 +137,13 @@ public class App {
     }
 
 
+    private static boolean isOpcaoValida(String opcao) {
+        if("1".equals(opcao) || "2".equals(opcao) || "3".equals(opcao) || "4".equals(opcao) || "5".equals(opcao)){
+            return true;
+        }
+        return false;
+    }
+
+
 }
+
